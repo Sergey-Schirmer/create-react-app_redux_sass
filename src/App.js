29c4470ef03d7898959e4accf579e1b1ebf7f92f
Sergey-Schirmer <ya.sch1987@yandex.ru>
2017-09-24 20:32:38 +0300

    add why did update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import NewApp from "./provider/NewApp";
+import {whyDidYouUpdate} from 'why-did-you-update';
 
-
+whyDidYouUpdate(React);
 class App extends Component {
   render() {
     return (
